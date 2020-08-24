2edad299a1f4bf7f09bdfc2fc9cd45df397c24de
Arni Medic <arni.medic@outlook.dk>
2020-08-24 19:50:51 +0200

    Initial Commit

---
+++ Todoaaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoaaa
+//
+//  Created by Arni Medic on 24/08/2020.
+//  Copyright © 2020 Arni Medic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
